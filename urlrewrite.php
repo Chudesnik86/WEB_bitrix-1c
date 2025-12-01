55d3a7b5382a89785227ce88dcf52b8923d0f187
Chudesnik222 <lexchudnetsov@gmail.com>
2025-12-01 23:43:16 +0500

    туда сюда

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,27 +1,16 @@
 <?php
 $arUrlRewrite=array (
-  0 => 
-  array (
-    'CONDITION' => '#^/services/#',
-    'RULE' => '',
-    'ID' => 'bitrix:catalog',
-    'PATH' => '/services/index.php',
-    'SORT' => 100,
-  ),
-  1 => 
-  array (
-    'CONDITION' => '#^/products/#',
-    'RULE' => '',
-    'ID' => 'bitrix:catalog',
-    'PATH' => '/products/index.php',
-    'SORT' => 100,
-  ),
-  2 => 
-  array (
-    'CONDITION' => '#^/news/#',
-    'RULE' => '',
-    'ID' => 'bitrix:news',
-    'PATH' => '/news/index.php',
-    'SORT' => 100,
-  ),
+    0 => array(
+        'CONDITION' => '#^/api/(.*)/(.*)/(.*)#',
+        'RULE' => 'CLASS=$1&METHOD=$2',
+        'ID' => 'legacy:api',
+        'PATH' => '/local/api/index.php',
+        'SORT' => 100,
+    ),
+
+    1 =>
+        array(
+            'CONDITION' => '#^(.*)#',
+            'PATH' => '/app/index.php',
+        ),
 );
